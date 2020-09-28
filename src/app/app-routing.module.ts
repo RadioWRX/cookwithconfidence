3ca7ec36db8210c7d5b050f2ad8d931adef12fad
Robert Martin <robertmartinbc@gmail.com>
2020-09-27 17:15:14 -0700

    Added Andorra and audio for anthems.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home/home.component';
 import { AfghanistanComponent } from './afghanistan/afghanistan.component';
 import { AlbaniaComponent } from './albania/albania.component';
 import { AlgeriaComponent } from './algeria/algeria.component';
+import { AndorraComponent } from './andorra/andorra.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'afghanistan', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'afghanistan', component: AfghanistanComponent },
   { path: 'albania', component: AlbaniaComponent },
-  { path: 'algeria', component: AlgeriaComponent }
+  { path: 'algeria', component: AlgeriaComponent },
+  { path: 'andorra', component: AndorraComponent }
 ];
 
 @NgModule({

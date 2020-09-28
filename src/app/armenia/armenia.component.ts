1ea20924adb9fb032ca3130ac735198fad9edfa6
Robert Martin <robertmartinbc@gmail.com>
2020-09-28 14:54:29 -0700

    Added bootstrap icons.

---
+++ src/app/armenia/armenia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-armenia',
+  templateUrl: './armenia.component.html',
+  styleUrls: ['./armenia.component.scss']
+})
+export class ArmeniaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

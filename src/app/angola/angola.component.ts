a5b5646fd12b6ddd0a4038594e98678611d03052
Robert Martin <robertmartinbc@gmail.com>
2020-10-23 18:13:57 -0700

    Updated more countries.

--- src/app/angola/angola.component.ts
+++ src/app/angola/angola.component.ts
@@ -1,12 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Params } from '@angular/router';
 
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 @Component({
   selector: 'app-angola',
   templateUrl: './angola.component.html',
   styleUrls: ['./angola.component.scss']
 })
 export class AngolaComponent implements OnInit {
+
+  customOptions1: any = {
+  loop: false,
+  margin: 10,
+  touchDrag:true,
+  autoplay:false,
+  responsiveClass: true,
+  navText: ['Previous', 'Next'],
+  responsive: {
+    0: {
+     items: 3
+   },
+    480: {
+     items: 5
+   },
+    940: {
+     items: 8
+   }
+  },
+ nav: false
+}
+
+customOptions2: any = {
+loop: true,
+margin: 10,
+touchDrag:true,
+autoplay:true,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
+
+customOptions3: any = {
+loop: false,
+margin: 10,
+touchDrag:true,
+autoplay:false,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
 
   constructor(
     public router: Router
--- src/app/argentina/argentina.component.ts
+++ src/app/argentina/argentina.component.ts
@@ -1,12 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Params } from '@angular/router';
 
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 @Component({
   selector: 'app-argentina',
   templateUrl: './argentina.component.html',
   styleUrls: ['./argentina.component.scss']
 })
 export class ArgentinaComponent implements OnInit {
+
+  customOptions1: any = {
+  loop: false,
+  margin: 10,
+  touchDrag:true,
+  autoplay:false,
+  responsiveClass: true,
+  navText: ['Previous', 'Next'],
+  responsive: {
+    0: {
+     items: 3
+   },
+    480: {
+     items: 5
+   },
+    940: {
+     items: 8
+   }
+  },
+ nav: false
+}
+
+customOptions2: any = {
+loop: true,
+margin: 10,
+touchDrag:true,
+autoplay:true,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
+
+customOptions3: any = {
+loop: false,
+margin: 10,
+touchDrag:true,
+autoplay:false,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
 
   constructor(
     public router: Router

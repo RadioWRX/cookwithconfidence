fe48fb82b79f5546eddcb6d0dd5ec8c6c3eea105
Robert Martin <robertmartinbc@gmail.com>
2020-11-03 22:03:54 -0800

    Completed the Bs

--- src/app/burundi/burundi.component.ts
+++ src/app/burundi/burundi.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Params } from '@angular/router';
+
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-burundi',
@@ -6,6 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./burundi.component.scss']
 })
 export class BurundiComponent implements OnInit {
+
+  customOptions1: any = {
+  loop: false,
+  margin: 10,
+  touchDrag:true,
+  autoplay:false,
+  responsiveClass: true,
+  navText: ['Previous', 'Next'],
+  responsive: {
+    0: {
+     items: 3
+   },
+    480: {
+     items: 5
+   },
+    940: {
+     items: 8
+   }
+  },
+ nav: false
+}
+
+customOptions2: any = {
+loop: true,
+margin: 10,
+touchDrag:true,
+autoplay:true,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
+
+customOptions3: any = {
+loop: false,
+margin: 10,
+touchDrag:true,
+autoplay:false,
+responsiveClass: true,
+navText: ['Previous', 'Next'],
+responsive: {
+  0: {
+   items: 1
+ },
+  480: {
+   items: 1
+ },
+  940: {
+   items: 1
+ }
+},
+nav: false
+}
 
   constructor() { }
 
